d80cfc42b27f5733ecd3ed3bfbdeb535ba77b329
ToniRib <tonimarierib@gmail.com>
2015-09-29 10:46:02 -0600

    Adds functionality for tic-tac-toe up through step 6 of the assignment

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -1,41 +1,66 @@
 # tictactoe.rb
 
-class Board
-  INITIAL_MARKER = ' '
+require 'pry'
 
+class Board
   def initialize
     @squares = {}
-    (1..9).each { |key| @squares[key] = INITIAL_MARKER }
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 
   def get_square_at(key)
     @squares[key]
   end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
 end
 
 class Square
-  def initialize(marker)
+  INITIAL_MARKER = ' '
+
+  attr_accessor :marker
+
+  def initialize(marker=INITIAL_MARKER)
     @marker = marker
   end
 
   def to_s
     @marker
   end
+
+  def unmarked?
+    marker == INITIAL_MARKER
+  end
 end
 
 class Player
-  def initialize
-  end
+  attr_reader :marker
 
-  def mark
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 class TTTGame
-  attr_reader :board
+  HUMAN_MARKER = 'X'
+  COMPUTER_MARKER = 'O'
+
+  attr_reader :board, :human, :computer
 
   def initialize
     @board = Board.new
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
   end
 
   def display_welcome_message
@@ -48,6 +73,8 @@ class TTTGame
   end
 
   def display_board
+    system 'clear'
+    puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
     puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
@@ -63,15 +90,39 @@ class TTTGame
     puts ""
   end
 
+  def human_moves
+    puts "Choose a square (#{board.unmarked_keys.join(', ')}):"
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if board.unmarked_keys.include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    board.set_square_at(square, human.marker)
+  end
+
+  def computer_moves
+    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+  end
+
+  def display_result
+    display_board
+    puts "The board is full!"
+  end
+
   def play
     display_welcome_message
+    display_board
     loop do
-      display_board
-      first_player_moves
-      break if someone_won? || board_full?
+      human_moves
+      break if board.full?
+      # break if someone_won? || board_full?
 
-      second_player_moves
-      break if someone_won? || board_full?
+      computer_moves
+      break if board.full?
+      display_board
+      # break if someone_won? || board_full?
     end
     display_result
     display_goodbye_message
