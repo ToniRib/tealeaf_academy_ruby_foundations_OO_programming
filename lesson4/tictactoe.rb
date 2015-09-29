f4b7814ae5f54c233625e4f3009dc7867ee6eb6c
ToniRib <tonimarierib@gmail.com>
2015-09-29 11:00:59 -0600

    Write detect_winner logic for tic-tac-toe

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -3,6 +3,10 @@
 require 'pry'
 
 class Board
+  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                  [[1, 5, 9], [3, 5, 7]]              # diagonals
+
   def initialize
     @squares = {}
     (1..9).each { |key| @squares[key] = Square.new }
@@ -22,6 +26,25 @@ class Board
 
   def full?
     unmarked_keys.empty?
+  end
+
+  def someone_won?
+    !!detect_winner
+  end
+
+  def detect_winner
+    WINNING_LINES.each do |line|
+      if @squares[line[0]].marker == TTTGame::HUMAN_MARKER &&
+         @squares[line[1]].marker == TTTGame::HUMAN_MARKER &&
+         @squares[line[2]].marker == TTTGame::HUMAN_MARKER
+        return TTTGame::HUMAN_MARKER
+      elsif @squares[line[0]].marker == TTTGame::COMPUTER_MARKER &&
+            @squares[line[1]].marker == TTTGame::COMPUTER_MARKER &&
+            @squares[line[2]].marker == TTTGame::COMPUTER_MARKER
+        return TTTGame::COMPUTER_MARKER
+      end
+    end
+    nil
   end
 end
 
@@ -108,7 +131,14 @@ class TTTGame
 
   def display_result
     display_board
-    puts "The board is full!"
+    case board.detect_winner
+    when human.marker
+      puts "You won!"
+    when computer.marker
+      puts "Computer won!"
+    else
+      puts "It's a tie!"
+    end
   end
 
   def play
@@ -116,13 +146,11 @@ class TTTGame
     display_board
     loop do
       human_moves
-      break if board.full?
-      # break if someone_won? || board_full?
+      break if board.someone_won? || board.full?
 
       computer_moves
-      break if board.full?
+      break if board.someone_won? || board.full?
       display_board
-      # break if someone_won? || board_full?
     end
     display_result
     display_goodbye_message
