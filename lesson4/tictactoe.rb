21d8e2e8e6ee142000dbfbe5b35f57500ae1b426
ToniRib <tonimarierib@gmail.com>
2015-09-29 12:06:55 -0600

    Updates name of detect_winner to winning_marker in tic-tac-toe

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -33,7 +33,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -44,7 +44,7 @@ class Board
     squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
   end
 
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
       if count_human_marker(@squares.values_at(*line)) == 3
         return TTTGame::HUMAN_MARKER
@@ -103,8 +103,7 @@ class TTTGame
     puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
 
-  def display_board(options = {clear_screen: true})
-    clear if options[:clear_screen]
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ''
     puts '     |     |'
@@ -119,6 +118,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts '     |     |'
     puts ''
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -138,8 +142,8 @@ class TTTGame
   end
 
   def display_result
-    display_board
-    case board.detect_winner
+    clear_screen_and_display_board
+    case board.winning_marker
     when human.marker
       puts 'You won!'
     when computer.marker
@@ -170,7 +174,7 @@ class TTTGame
     display_welcome_message
 
     loop do
-      display_board(clear_screen: false)
+      display_board
 
       loop do
         human_moves
@@ -179,7 +183,7 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
 
       display_result
