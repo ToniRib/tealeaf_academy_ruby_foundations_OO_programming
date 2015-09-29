644ee9246479f9267d34d118a9b22c838b0717a8
ToniRib <tonimarierib@gmail.com>
2015-09-29 12:11:58 -0600

    Moves logic for drawing board into the board class for tic-tac-toe

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -12,6 +12,20 @@ class Board
     reset
   end
 
+  def draw
+    puts '     |     |'
+    puts "  #{get_square_at(1)}  |  #{get_square_at(2)}  |  #{get_square_at(3)}"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
+    puts "  #{get_square_at(4)}  |  #{get_square_at(5)}  |  #{get_square_at(6)}"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
+    puts "  #{get_square_at(7)}  |  #{get_square_at(8)}  |  #{get_square_at(9)}"
+    puts '     |     |'
+  end
+
   def reset
     (1..9).each { |key| @squares[key] = Square.new }
   end
@@ -106,17 +120,7 @@ class TTTGame
   def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ''
-    puts '     |     |'
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts '     |     |'
-    puts '-----+-----+-----'
-    puts '     |     |'
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
-    puts '     |     |'
-    puts '-----+-----+-----'
-    puts '     |     |'
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
-    puts '     |     |'
+    board.draw
     puts ''
   end
 
